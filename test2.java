a5d2ef036fadb36649e8d82e5ee56b313ed9494f
cts316 <34383221+cts316@users.noreply.github.com>
2017-12-08 13:59:15 -0800

    made test2.java
    new java file

---
+++ test2.java
@@ -0,0 +1,9 @@
+
+public class test {
+    public static void main (String[] args) {
+        String outputPrint = "Hello, World!";
+        for(int counter = 0; counter < 10; counter++) {
+            System.out.println(counter + ": " + outputPrint);
+        }
+    }
+}
